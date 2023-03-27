68c9604c4df7211f4fa4da29bbec0467688ae536
Marcos <marcos.henrique.a7x@@gmail.com>
2023-03-27 16:33:46 -0300

    minha primeira API

---
+++ Routes/PersonRouters.js
@@ -0,0 +1,99 @@
+const router = require("express").Router();
+const Person = require("../models/Person");
+
+// create
+router.post("/", async (req, res) => {
+  const { name, salary, approved } = req.body;
+
+  if (!name) {
+    res.status(422).json({ error: "O nome é obrigatório!" });
+    return;
+  }
+
+  const person = {
+    name,
+    salary,
+    approved,
+  };
+
+  try {
+    await Person.create(person);
+
+    res
+      .status(201)
+      .json({ message: "Pessoa inserida no sistema com sucesso!" });
+  } catch (error) {
+    res.status(500).json({ erro: error });
+  }
+});
+
+// read
+
+router.get("/", async (req, res) => {
+  try {
+    const people = await Person.find();
+    res.status(200).json(people);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  //   console.log("req", req);
+
+  const id = req.params.id;
+  try {
+    const person = await Person.findOne({ _id: id });
+
+    if (!person) {
+      res.status(422).json({ message: "o usuário não foi encontrado" });
+      return;
+    }
+    res.status(200).json(person);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+// atualização PUT -> atualiza todo o dado - PATH -> Atualização parcial
+router.patch("/:id", async (req, res) => {
+  const id = req.params.id;
+  const { name, salary, approved } = req.body;
+  const person = {
+    name,
+    salary,
+    approved,
+  };
+
+  try {
+    const updatePerson = await Person.updateOne({ _id: id }, person);
+    // console.log(updatePerson)
+    if (updatePerson.matchedCount === 0) {
+      // se não localizar id para atualizar
+      res.status(422).json({ message: "o usuário não foi encontrado" });
+      return;
+    }
+    res.status(200).json(person);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const person = await Person.findOne({ _id: id });
+
+  if (!person) {
+    res.status(422).json({ message: "o usuário não foi encontrado" });
+    return;
+  }
+
+  try {
+    await Person.deleteOne({ _id: id });
+    res.status(200).json({ message: "usuário removido com sucesso" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,38 @@
+// config inicial
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// depois do db
+const mongoose = require("mongoose");
+
+app.use(cors());
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+// rotas
+const personRouters = require("./Routes/PersonRouters");
+app.use("/person", personRouters);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Oi Express!" });
+});
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+const conexaoBanco = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.5fnzkhj.mongodb.net/test`;
+mongoose
+  .connect(conexaoBanco)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("server online");
+    });
+  })
+  .catch((err) => console.log(err));
